32b62fb5b8c8bdcda580c6dc47fe25df0a0b1ff6
jb <jon.baker1222@gmail.com>
2020-05-02 02:27:35 -0400

    gui basics

--- main.cpp
+++ main.cpp
@@ -46,7 +46,7 @@ int main(int, char**)
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_BORDERLESS | SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
     SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
     SDL_GL_MakeCurrent(window, gl_context);
@@ -78,14 +78,18 @@ int main(int, char**)
     ImGui_ImplOpenGL3_Init(glsl_version);
 
     // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    ImVec4 clear_color = ImVec4(48.0f/256.0f, 7.0f/256.0f, 17.0f/256.0f, 1.00f);
+
 
     // Main loop
     bool done = false;
     while (!done)
     {
+        // Poll and handle events (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
@@ -105,42 +109,91 @@ int main(int, char**)
         ImGui_ImplSDL2_NewFrame(window);
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        //start control window
+        static float timescale = 0.0f;
+        static float gravity = 10.0f;
+        static float noise_scale = 1.0f;
+        static bool run_simulation = false;
+        static bool show_controls = true;
 
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        ImGui::SetNextWindowSize(ImVec2(315,465));
+        ImGui::Begin("Controls", &show_controls);             
+
+        ImGui::Checkbox("Run simulation", &run_simulation);  
+
+        ImGui::SameLine();
+        if(ImGui::Button("Single Step"))
         {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
+            //call the update function
+        }
+        
+        ImGui::SameLine();
+        if(ImGui::Button("Reset"))
+        {
+            //call the reinit function
         }
 
-        // 3. Show another simple window.
-        //if (show_another_window)
-        //{
-        //  ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-        //    ImGui::Text("Hello from another window!");
-        //    if (ImGui::Button("Close Me"))
-        //        show_another_window = false;
-        //   ImGui::End();
-        //}
+        ImGui::Separator();
+        ImGui::Text("Simulation general settings");
+        ImGui::Separator();
+        ImGui::SetCursorPosX(25); 
+        ImGui::SliderFloat("time step", &timescale, 0.0f, 0.1f);
+        ImGui::SetCursorPosX(25);
+        ImGui::SliderFloat("gravity", &gravity, -5.0f, 10.0f);
+        ImGui::SetCursorPosX(25);
+        ImGui::SliderFloat("noise scale", &noise_scale, 0, 5.0f);
+
+        static float chassis_k;
+        static float chassis_damp;
+
+        static ImVec4 chassis_tension_color = {0x45,0x23,0x0,0xff};
+        static ImVec4 chassis_compression_color = {0x0,0x23,0x43,0xff};
+
+        ImGui::Separator();
+        ImGui::Text("Chassis spring/damp constants  ");
+        ImGui::SetCursorPosX(25);
+        ImGui::ColorEdit4("tension", (float*)&chassis_tension_color, ImGuiColorEditFlags_PickerHueWheel | ImGuiColorEditFlags_AlphaBar | ImGuiColorEditFlags_AlphaPreviewHalf);
+        ImGui::SetCursorPosX(25);
+        ImGui::ColorEdit4("compression", (float*)&chassis_compression_color, ImGuiColorEditFlags_PickerHueWheel | ImGuiColorEditFlags_AlphaBar | ImGuiColorEditFlags_AlphaPreviewHalf);
+
+        ImGui::SetCursorPosX(25);
+        ImGui::SliderFloat("k", &chassis_k, 0.0f, 100.0f);
+        ImGui::SetCursorPosX(25);
+        ImGui::SliderFloat("damp", &chassis_damp, 0.0f, 40.0f);
+
+
+
+        static float suspension_k;
+        static float suspension_damp;
+        
+        static ImVec4 suspension_tension_color = {0x11,0x45,0x2,0xff};
+        static ImVec4 suspension_compression_color = {0x45,0x0,0x53,0xff};
+
+        ImGui::Separator();
+        ImGui::Text("Suspension spring/damp constants"); 
+        ImGui::SetCursorPosX(25);
+        ImGui::ColorEdit4("tension", (float*)&suspension_tension_color, ImGuiColorEditFlags_PickerHueWheel | ImGuiColorEditFlags_AlphaBar | ImGuiColorEditFlags_AlphaPreviewHalf);
+        ImGui::SetCursorPosX(25);
+        ImGui::ColorEdit4("compression", (float*)&suspension_compression_color, ImGuiColorEditFlags_PickerHueWheel | ImGuiColorEditFlags_AlphaBar | ImGuiColorEditFlags_AlphaPreviewHalf);
+
+        ImGui::SetCursorPosX(25);
+        ImGui::SliderFloat("k", &suspension_k, 0.0f, 100.0f);
+        ImGui::SetCursorPosX(25);
+        ImGui::SliderFloat("damp", &suspension_damp, 0.0f, 40.0f);
+
+        ImGui::Text(" "); 
+        ImGui::Text(" "); 
+        ImGui::Text(" "); 
+
+
+
+
+
+
+        ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+        ImGui::Text("average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
 
         // Rendering
         ImGui::Render();
